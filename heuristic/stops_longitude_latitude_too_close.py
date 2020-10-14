32df6bab60f4b7cf39c948bf746a73c5d42814a8
Maxime Armstrong <info@maximearmstrong.com>
2020-10-14 12:26:59 -0400

    Documentation - Adds github action for Sphinx build, adds reST docs to stops_longitude_latitude_too_close.py

--- heuristic/stops_longitude_latitude_too_close.py
+++ heuristic/stops_longitude_latitude_too_close.py
@@ -5,6 +5,14 @@ class StopsLongitudeLatitudeTooClose:
     LATITUDE_DEGREE_IN_METER = 111319.5
 
     def execute(self, stop_1, stop_2, min_distance_meter=5.0):
+        """
+        Verifies if two stop are too close by comparing latitude and longitude.
+        :param stop_1: First stop to compare.
+        :param stop_2: Second stop to compare.
+        :param min_distance_meter: Minimum distance threshold in meter. Default at 5m.
+        :returns: `boolean`
+            True if the two stops are closer than the ``min_distance_meter``, false otherwise.
+        """
         if stop_1 is not None and stop_2 is not None:
             # Computing latitude distance first as it is a constant.
             # According to Wikipedia, 1 degree of latitude is 111319.5 meters
