c2c5cda0f8bcbb4b9826dbbbe4972d6c1ee42e83
Maxime Armstrong <info@maximearmstrong.com>
2020-11-03 18:07:39 -0500

    Download dataset modification - refactors DownloadDatasetAsZip

--- usecase/download_dataset_as_zip.py
+++ usecase/download_dataset_as_zip.py
@@ -2,16 +2,16 @@ import os
 import requests
 
 
-class DownloadDataset:
-    def __init__(self, data_folder_path, urls):
-        """Constructor for ``DownloadDataset``.
-        :param data_folder_path: The path to the folder where to store the datasets.
-        :param urls: URLs of the datasets to download.
+class DownloadDatasetAsZip:
+    def __init__(self, path_to_data, urls):
+        """Constructor for ``DownloadDatasetAsZip``.
+        :param path_to_data: The path to the folder where to store the dataset zip files.
+        :param urls: URLs of the dataset zip files to download.
         """
         try:
-            if data_folder_path is None or not os.path.isdir(data_folder_path):
-                raise TypeError("Temporary data path must be a valid path.")
-            self.data_folder_path = data_folder_path
+            if path_to_data is None or not os.path.isdir(path_to_data):
+                raise TypeError("Data path must be a valid path.")
+            self.data_folder_path = path_to_data
             if urls is None or not isinstance(urls, dict):
                 raise TypeError("URLs must be a valid dictionary.")
             self.urls = urls
@@ -19,8 +19,8 @@ class DownloadDataset:
             raise e
 
     def execute(self):
-        """Execute the ``DownloadDataset`` use case.
-        :return: The paths to the downloaded datasets.
+        """Execute the ``DownloadDatasetAsZip`` use case.
+        :return: The paths to the downloaded dataset zip files.
         """
         zip_paths = {}
         for entity_code, url in self.urls.items():
