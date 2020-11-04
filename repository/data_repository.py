4deb961b669b9bd41f291f444c68eae95cfd3047
Maxime Armstrong <info@maximearmstrong.com>
2020-11-04 11:48:08 -0500

    Load dataset - modifies DataRepository documentation

--- repository/data_repository.py
+++ repository/data_repository.py
@@ -2,14 +2,12 @@
 
 class DataRepository:
     def __init__(self):
-        """
-        Constructor for ``DataRepository``.
+        """Constructor for ``DataRepository``.
         """
         self.__dataset_representations = {}
 
     def add_dataset_representation(self, dataset_key, dataset_representation):
-        """
-        Add a dataset representation to the repository.
+        """Add a dataset representation to the repository.
         :param dataset_key: Key to access the dataset representation in the repository.
         :param dataset_representation: The dataset representation to add to the repository.
         """
@@ -18,16 +16,14 @@ class DataRepository:
         self.__dataset_representations[dataset_key] = dataset_representation
 
     def print_dataset_representations(self):
-        """
-        Print the dataset representations in the repository.
+        """Print the dataset representations in the repository.
         """
         for key, representation in self.__dataset_representations.items():
             print("--------------- Dataset representation for entity %s ---------------\n" % key)
             representation.print_representation()
 
     def print_dataset_representation(self, dataset_key):
-        """
-        Print the dataset representation accessible with the given key.
+        """Print the dataset representation accessible with the given key.
         :param dataset_key: Key to access the dataset representation in the repository.
         """
         if dataset_key in self.__dataset_representations:
