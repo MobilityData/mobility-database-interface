2bc513e744ca62cd3b12e6f1bf36c04e0d9201df
Maxime Armstrong <info@maximearmstrong.com>
2021-01-26 12:49:27 -0500

    Refactors process_bounding_octagon_for_gtfs_metadata and test_process_bounding_octagon_for_gtfs_metadata

--- usecase/process_bounding_octagon_for_gtfs_metadata.py
+++ usecase/process_bounding_octagon_for_gtfs_metadata.py
@@ -1,48 +1,57 @@
 from representation.gtfs_representation import GtfsRepresentation
 from utilities.geographical_utils import *
 
+RIGHT_BOTTOM_CORNER_KEY = "1"
+BOTTOM_RIGHT_CORNER_KEY = "2"
+BOTTOM_LEFT_CORNER_KEY = "3"
+LEFT_BOTTOM_CORNER_KEY = "4"
+LEFT_TOP_CORNER_KEY = "5"
+TOP_LEFT_CORNER_KEY = "6"
+TOP_RIGHT_CORNER_KEY = "7"
+RIGHT_TOP_CORNER_KEY = "8"
+BOUNDING_BOX_SETTER = "set_metadata_bounding_box"
 
-class ProcessBoundingOctagonForGtfsMetadata:
-    def __init__(self, gtfs_representation):
-        """Constructor for ``ProcessBoundingOctagonForGtfsMetadata``.
-        :param gtfs_representation: The representation of the GTFS dataset to process.
-        """
-        try:
-            if gtfs_representation is None or not isinstance(gtfs_representation, GtfsRepresentation):
-                raise TypeError("GTFS data representation must be a valid GtfsRepresentation.")
-            self.gtfs_representation = gtfs_representation
-        except Exception as e:
-            raise e
 
-    def execute(self):
-        """Execute the ``ProcessBoundingOctagonForGtfsMetadata`` use case.
-        Process the bounding octagon using the`stops` file from the GTFS dataset of the representation.
-        Add the bounding octagon to the representation metadata once processed.
-        :return: The representation of the GTFS dataset post-execution.
-        """
-        dataset = self.gtfs_representation.get_dataset()
+def process_bounding_octagon_for_gtfs_metadata(gtfs_representation):
+    """Execute the ``ProcessBoundingOctagonForGtfsMetadata`` use case.
+    Process the bounding octagon using the`stops` file from the GTFS dataset of the representation.
+    Add the bounding octagon to the representation metadata once processed.
+    :param gtfs_representation: The representation of the GTFS dataset to process.
+    :return: The representation of the GTFS dataset post-execution.
+    """
+    if not isinstance(gtfs_representation, GtfsRepresentation):
+        raise TypeError("GTFS data representation must be a valid GtfsRepresentation.")
+    dataset = gtfs_representation.get_dataset()
 
-        # Extract the octagon corners coordinates in the dataset representation
-        octagon_corners = process_bounding_octagon_corner_strings(dataset)
+    # Extract the octagon corners coordinates in the dataset representation
+    (
+        right_bottom_corner,
+        bottom_right_corner,
+        bottom_left_corner,
+        left_bottom_corner,
+        left_top_corner,
+        top_left_corner,
+        top_right_corner,
+        right_top_corner
+    ) = process_bounding_octagon_corner_strings(dataset)
 
-        right_bottom_corner = octagon_corners[0]
-        bottom_right_corner = octagon_corners[1]
-        bottom_left_corner = octagon_corners[2]
-        left_bottom_corner = octagon_corners[3]
-        left_top_corner = octagon_corners[4]
-        top_left_corner = octagon_corners[5]
-        top_right_corner = octagon_corners[6]
-        right_top_corner = octagon_corners[7]
+    # Order the corners inside a bounding octagon
+    bounding_octagon = {RIGHT_BOTTOM_CORNER_KEY: right_bottom_corner,
+                        BOTTOM_RIGHT_CORNER_KEY: bottom_right_corner,
+                        BOTTOM_LEFT_CORNER_KEY: bottom_left_corner,
+                        LEFT_BOTTOM_CORNER_KEY: left_bottom_corner,
+                        LEFT_TOP_CORNER_KEY: left_top_corner,
+                        TOP_LEFT_CORNER_KEY: top_left_corner,
+                        TOP_RIGHT_CORNER_KEY: top_right_corner,
+                        RIGHT_TOP_CORNER_KEY: right_top_corner}
 
-        # Order the corners inside a bounding octagon
-        bounding_octagon = {"1": right_bottom_corner,
-                            "2": bottom_right_corner,
-                            "3": bottom_left_corner,
-                            "4": left_bottom_corner,
-                            "5": left_top_corner,
-                            "6": top_left_corner,
-                            "7": top_right_corner,
-                            "8": right_top_corner}
+    gtfs_representation.set_metadata_bounding_octagon(bounding_octagon)
+    return gtfs_representation
 
-        self.gtfs_representation.set_metadata_bounding_octagon(bounding_octagon)
-        return self.gtfs_representation
+
+
+
+
+
+
+
