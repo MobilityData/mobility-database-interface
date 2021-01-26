2da84802864d4aaa17790243b88f747358373225
Annaelle <annaelle.duff@gmail.com>
2021-01-26 14:47:20 -0600

    Fixes #85: refactoring process_main_timezone...py

--- usecase/test/test_process_main_timezone_for_gtfs_metadata.py
+++ usecase/test/test_process_main_timezone_for_gtfs_metadata.py
@@ -4,33 +4,33 @@ from unittest.mock import MagicMock, PropertyMock
 from gtfs_kit.feed import Feed
 from representation.gtfs_metadata import GtfsMetadata
 from representation.gtfs_representation import GtfsRepresentation
-from usecase.process_main_timezone_for_gtfs_metadata import ProcessMainTimezoneForGtfsMetadata
+from usecase.process_main_timezone_for_gtfs_metadata import process_main_timezone_for_gtfs_metadata
 
 
 class ProcessMainTimezoneForGtfsMetadataTest(TestCase):
 
-    def test_process_main_timezone_with_none_gtfs_representation_should_raise_exception(self):
-        self.assertRaises(TypeError, ProcessMainTimezoneForGtfsMetadata, None)
+    def test_process_main_timezone_with_none(self):
+        self.assertRaises(TypeError, process_main_timezone_for_gtfs_metadata, None)
 
-    def test_process_main_timezone_with_invalid_gtfs_representation_should_raise_exception(self):
+    def test_process_main_timezone_with_invalid_gtfs_repr(self):
         mock_gtfs_representation = MagicMock()
         mock_gtfs_representation.__class__ = str
-        self.assertRaises(TypeError, ProcessMainTimezoneForGtfsMetadata, mock_gtfs_representation)
+        self.assertRaises(TypeError, process_main_timezone_for_gtfs_metadata, mock_gtfs_representation)
 
     @mock.patch('representation.gtfs_representation.GtfsRepresentation')
-    def test_process_main_timezone_with_valid_gtfs_representation_should_return_instance(self,
-                                                                                         mock_gtfs_representation):
+    def test_process_main_timezone_with_valid_gtfs_representation(self,
+                                                                 mock_gtfs_representation):
         mock_gtfs_representation.__class__ = GtfsRepresentation
-        under_test = ProcessMainTimezoneForGtfsMetadata(mock_gtfs_representation)
-        self.assertIsInstance(under_test, ProcessMainTimezoneForGtfsMetadata)
+        under_test = process_main_timezone_for_gtfs_metadata(mock_gtfs_representation)
+        self.assertIsInstance(under_test, GtfsRepresentation)
 
     @mock.patch('representation.gtfs_representation.GtfsRepresentation')
     @mock.patch('gtfs_kit.feed.Feed')
     @mock.patch('representation.gtfs_metadata.GtfsMetadata')
-    def test_process_main_timezone_with_gtfs_dataset_should_add_main_timezone_to_gtfs_metadata(self,
-                                                                                               mock_gtfs_representation,
-                                                                                               mock_dataset,
-                                                                                               mock_metadata):
+    def test_process_main_timezone(self,
+                                   mock_gtfs_representation,
+                                   mock_dataset,
+                                   mock_metadata):
         mock_agency = PropertyMock(return_value=pd.DataFrame({'agency_timezone': ['America/Montreal']}))
         mock_dataset.__class__ = Feed
         type(mock_dataset).agency = mock_agency
@@ -39,7 +39,7 @@ class ProcessMainTimezoneForGtfsMetadataTest(TestCase):
         mock_gtfs_representation.__class__ = GtfsRepresentation
         mock_gtfs_representation.get_dataset.return_value = mock_dataset
 
-        under_test = ProcessMainTimezoneForGtfsMetadata(mock_gtfs_representation).execute()
+        under_test = process_main_timezone_for_gtfs_metadata(mock_gtfs_representation)
         self.assertIsInstance(under_test, GtfsRepresentation)
         mock_gtfs_representation.get_dataset.assert_called_once()
         mock_agency.assert_called()
