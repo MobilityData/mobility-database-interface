ecafb450126597ca1f373bf04af5450a53a96d56
Maxime Armstrong <info@maximearmstrong.com>
2021-02-02 13:40:01 -0500

    Modifies process_all_timezones_for_gtfs_metadata.py and test_process_all_timezones_for_gtfs_metadata.py after review

--- usecase/process_all_timezones_for_gtfs_metadata.py
+++ usecase/process_all_timezones_for_gtfs_metadata.py
@@ -1,5 +1,8 @@
 from representation.gtfs_representation import GtfsRepresentation
 
+STOP_TIMEZONE_KEY = "stop_timezone"
+AGENCY_TIMEZONE_KEY = "agency_timezone"
+
 
 def process_all_timezones_for_gtfs_metadata(gtfs_representation):
     """Process all the timezones using the `stops` and the `agency` files from the GTFS dataset of the representation.
@@ -7,22 +10,22 @@ def process_all_timezones_for_gtfs_metadata(gtfs_representation):
     :param gtfs_representation: The representation of the GTFS dataset to process.
     :return: The representation of the GTFS dataset post-execution.
     """
-    if gtfs_representation is None or not isinstance(gtfs_representation, GtfsRepresentation):
+    if not isinstance(gtfs_representation, GtfsRepresentation):
         raise TypeError("GTFS data representation must be a valid GtfsRepresentation.")
     dataset = gtfs_representation.get_dataset()
 
     # Extract the all the timezones using the stop_timezone in the dataset stops
     all_timezones = set()
-    if 'stop_timezone' in dataset.stops.columns:
+    if STOP_TIMEZONE_KEY in dataset.stops.columns:
         for index, row in dataset.stops.iterrows():
-            if row['stop_timezone'] is not None:
-                all_timezones.add(row['stop_timezone'])
+            if row[STOP_TIMEZONE_KEY] is not None:
+                all_timezones.add(row[STOP_TIMEZONE_KEY])
 
     # Extract the timezone from the first row in the dataset agency
     # to add the main timezone to the set
     # if the set of all timezones is empty after processing the dataset stops
     if len(all_timezones) == 0:
-        all_timezones.add(dataset.agency['agency_timezone'].iloc[0])
+        all_timezones.add(dataset.agency[AGENCY_TIMEZONE_KEY].iloc[0])
 
     # Convert the set of time to a list, and sort it alphabetically
     all_timezones = sorted(list(all_timezones))
