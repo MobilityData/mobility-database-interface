232503cc89e1151bc444d2bd48567ee43f12c896
Maxime Armstrong <info@maximearmstrong.com>
2021-03-03 13:27:00 -0500

    Refactors process_md5.py

--- usecase/process_md5.py
+++ usecase/process_md5.py
@@ -12,12 +12,13 @@ def process_md5(datasets_infos):
     :return: The datasets infos of the datasets for which the MD5 hashes are not in the database.
     """
     validate_datasets_infos(datasets_infos)
+    updated_datasets_infos = []
 
     # Decrementing the index to ensure no out of bound exception after the datasets_infos.pop(index)
-    for index in range(len(datasets_infos) - 1, -1, -1):
+    for dataset_infos in datasets_infos:
         md5_hash = md5()
-        path_to_dataset = datasets_infos[index].zip_path
-        previous_md5_hashes = datasets_infos[index].previous_md5_hashes
+        path_to_dataset = dataset_infos.zip_path
+        previous_md5_hashes = dataset_infos.previous_md5_hashes
 
         print(f"--------------- Processing MD5 : {path_to_dataset} ---------------\n")
         try:
@@ -31,17 +32,14 @@ def process_md5(datasets_infos):
 
         md5_hash = md5_hash.hexdigest()
         if md5_hash not in previous_md5_hashes:
-            datasets_infos[index].md5_hash = md5_hash
+            dataset_infos.md5_hash = md5_hash
+            updated_datasets_infos.append(dataset_infos)
             print(
                 f"Success : new MD5 hash {md5_hash} for {path_to_dataset}, dataset kept for further processing\n"
             )
         else:
-            try:
-                datasets_infos.pop(index)
-                print(
-                    f"Success : MD5 hash {md5_hash} already exists for {path_to_dataset}, dataset discarded\n"
-                )
-            except ValueError:
-                pass
+            print(
+                f"Success : MD5 hash {md5_hash} already exists for {path_to_dataset}, dataset discarded\n"
+            )
 
-    return datasets_infos
+    return updated_datasets_infos
