1beddd8efc94f3f2871eb04068c1e97fd0084143
Maxime Armstrong <info@maximearmstrong.com>
2021-03-17 10:25:46 -0400

    Refactors main.py

--- main.py
+++ main.py
@@ -1,50 +1,16 @@
-from dotenv import load_dotenv
 import argparse
-import sys
+from dotenv import load_dotenv
 from guppy import hpy
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="MobilityDatabase Interface Script")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument(
+    parser.add_argument(
         "--data_type",
         action="store",
         choices=["GTFS", "GBFS"],
         default="GTFS",
         help='Type of the datasets to process. Possible values : "GTFS", "GBFS".',
-    )
-    group.add_argument(
-        "--download",
-        action="store_const",
-        const=True,
-        help="Download datasets in memory. Usage : --download (-a | -s SPECIFIC)",
-    )
-    group.add_argument(
-        "--load",
-        action="store",
-        help="Load a dataset in memory. Must include path "
-        "to the dataset zip file as positional argument.",
-    )
-    download_group = parser.add_mutually_exclusive_group(
-        required="--download" in sys.argv
-    )
-    download_group.add_argument(
-        "-a",
-        "--all",
-        action="store_const",
-        const=True,
-        help="Download all datasets found for a type in the Mobility Database."
-        'Required with --download to select the "Download all" option.',
-    )
-    download_group.add_argument(
-        "-s",
-        "--specific",
-        action="store",
-        help="Download the dataset related to an entity code in the Mobility Database. "
-        "Entity code for the specific dataset to download must be valid and provided "
-        "as positional argument. Required with --download to select "
-        'the "Download specific" option.',
     )
     parser.add_argument(
         "--path_to_tmp_data",
@@ -55,8 +21,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--path_to_env_var",
         action="store",
-        default="./data/tmp/",
-        help="Path to the folder where to temporary store downloaded datasets for processing.",
+        default="./.env.staging",
+        help="Path to the environment variables.",
     )
     args = parser.parse_args()
 
@@ -71,7 +37,6 @@ if __name__ == "__main__":
     )
     from usecase.extract_datasets_infos_from_database import (
         extract_gtfs_datasets_infos_from_database,
-        extract_gbfs_datasets_infos_from_database,
     )
     from usecase.load_dataset import load_dataset
     from usecase.process_all_timezones_for_gtfs_metadata import (
@@ -111,74 +76,68 @@ if __name__ == "__main__":
     data_repository = DataRepository()
 
     # Process data
-    if args.download is not None:
-        # Download datasets zip files
-        datasets_infos = extract_gtfs_datasets_infos_from_database(
-            API_URL,
-            SPARQL_URL,
+    # Download datasets zip files
+    datasets_infos = extract_gtfs_datasets_infos_from_database(
+        API_URL,
+        SPARQL_URL,
+    )
+
+    # Download datasets zip files
+    datasets_infos = download_dataset_as_zip(args.path_to_tmp_data, datasets_infos)
+
+    # Process the MD5 hashes
+    datasets_infos = process_md5(datasets_infos)
+
+    # Load the datasets in memory in the data repository
+    data_repository = load_dataset(data_repository, datasets_infos, args.data_type)
+
+    # Process each dataset representation in the data_repository
+    for (
+        dataset_key,
+        dataset_representation,
+    ) in data_repository.get_dataset_representations().items():
+        dataset_representation = process_start_service_date_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_end_service_date_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_start_timestamp_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_end_timestamp_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_main_language_code_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_main_timezone_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_all_timezones_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_bounding_box_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_bounding_octagon_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_agencies_count_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_routes_count_by_type_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = process_stops_count_by_type_for_gtfs_metadata(
+            dataset_representation
+        )
+        dataset_representation = create_dataset_entity_for_gtfs_metadata(
+            dataset_representation, API_URL
         )
 
-        # Download datasets zip files
-        datasets_infos = download_dataset_as_zip(args.path_to_tmp_data, datasets_infos)
-
-        # Process the MD5 hashes
-        datasets_infos = process_md5(datasets_infos)
-
-        # Load the datasets in memory in the data repository
-        data_repository = load_dataset(data_repository, datasets_infos, args.data_type)
-
-        # Process each dataset representation in the data_repository
-        for (
-            dataset_key,
-            dataset_representation,
-        ) in data_repository.get_dataset_representations().items():
-            dataset_representation = process_start_service_date_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_end_service_date_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_start_timestamp_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_end_timestamp_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_main_language_code_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_main_timezone_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_all_timezones_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_bounding_box_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_bounding_octagon_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_agencies_count_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_routes_count_by_type_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = process_stops_count_by_type_for_gtfs_metadata(
-                dataset_representation
-            )
-            dataset_representation = create_dataset_entity_for_gtfs_metadata(
-                dataset_representation, API_URL
-            )
-
-            # Print results
-            data_repository.print_dataset_representation(dataset_key)
-
-    elif args.load is not None:
-        # Load dataset in memory
-        # TODO dataset = load_data(args['load'])
-        pass
+        # Print results
+        data_repository.print_dataset_representation(dataset_key)
 
     # Print memory usage
     print("\n--------------- Memory Usage ---------------\n")
