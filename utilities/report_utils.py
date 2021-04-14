70eeac9384d32cc2b09182d52fa74a7060010cf6
Maxime Armstrong <info@maximearmstrong.com>
2021-04-14 00:26:00 -0400

    Modifies report_utils.py

--- utilities/report_utils.py
+++ utilities/report_utils.py
@@ -3,17 +3,37 @@ from utilities.validators import validate_report
 
 def clean_report(report):
     validate_report(report)
-    cleaned_report = {}
+    cleaned_report = {"standalone": set(), "with_file": {}}
     for notice_type in report.get("notices"):
         notice_code = notice_type.get("code")
-        cleaned_report[notice_code] = set()
         for notice in notice_type.get("notices"):
-            cleaned_report[notice_code].add(notice.get("filename", ""))
+            if "filename" in notice or "childFilename" in notice:
+                if "filename" in notice:
+                    filename = notice.get("filename")
+                else:
+                    filename = notice.get("childFilename")
+                if filename not in cleaned_report["with_file"]:
+                    cleaned_report["with_file"][filename] = set()
+                cleaned_report["with_file"][filename].add(notice_code)
+            else:
+                cleaned_report["standalone"].add(notice_code)
     return cleaned_report
 
 
 def merge_reports(validation_report, system_report):
-    return {**validation_report, **system_report}
+    report = {"standalone": set(), "with_file": {}}
+    report["standalone"].update(validation_report["standalone"])
+    report["standalone"].update(system_report["standalone"])
+
+    with_file_keys = set()
+    with_file_keys.update(validation_report["with_file"].keys())
+    with_file_keys.update(system_report["with_file"].keys())
+
+    for key in with_file_keys:
+        report["with_file"][key] = set()
+        report["with_file"][key].update(validation_report["with_file"].get(key, set()))
+        report["with_file"][key].update(system_report["with_file"].get(key, set()))
+    return report
 
 
 def apply_report_to_scenario(report, scenario):
