9c436720e00b0bf5299a7a232fa929e9dc9cc429
Maxime Armstrong <info@maximearmstrong.com>
2021-04-14 23:06:10 -0400

    Modifies test_report_utils.py

--- utilities/test/test_report_utils.py
+++ utilities/test/test_report_utils.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, mock
 
-from utilities.report_utils import clean_report
+from utilities.report_utils import clean_report, merge_reports
 
 
 class TestCleanReport(TestCase):
@@ -34,12 +34,12 @@ class TestCleanReport(TestCase):
         test_report = {
             "notices": [
                 {
-                    "code": "standalone",
+                    "code": "standalone_notice",
                     "notices": [{"duplicatedField": "duplicated_field"}],
                 },
-                {"code": "with_filename", "notices": [{"filename": "filename"}]},
+                {"code": "with_filename_notice", "notices": [{"filename": "filename"}]},
                 {
-                    "code": "with_child_filename",
+                    "code": "with_child_filename_notice",
                     "notices": [{"childFilename": "child_filename"}],
                 },
             ]
@@ -48,10 +48,60 @@ class TestCleanReport(TestCase):
         self.assertEqual(
             under_test,
             {
-                "standalone": {"standalone"},
+                "standalone": {"standalone_notice"},
                 "with_filename": {
-                    "filename": {"with_filename"},
-                    "child_filename": {"with_child_filename"},
+                    "filename": {"with_filename_notice"},
+                    "child_filename": {"with_child_filename_notice"},
+                },
+            },
+        )
+
+
+class TestMergeReports(TestCase):
+    def test_merge_empty_reports(self):
+        test_validation_report = {"standalone": set(), "with_filename": {}}
+        test_system_report = {"standalone": set(), "with_filename": {}}
+        under_test = merge_reports(test_validation_report, test_system_report)
+        self.assertEqual(under_test, {"standalone": set(), "with_filename": {}})
+
+    def test_merge_reports(self):
+        test_validation_report = {
+            "standalone": {"standalone_validation_notice"},
+            "with_filename": {
+                "filename": {"with_filename_validation_notice"},
+                "child_filename": {"with_child_filename_validation_notice"},
+                "another_filename_validation": {"another_validation_filename_notice"},
+            },
+        }
+        test_system_report = {
+            "standalone": {"standalone_system_notice"},
+            "with_filename": {
+                "filename": {"with_filename_system_notice"},
+                "child_filename": {"with_child_filename_system_notice"},
+                "another_filename_system": {"another_system_filename_notice"},
+            },
+        }
+        under_test = merge_reports(test_validation_report, test_system_report)
+        self.assertEqual(
+            under_test,
+            {
+                "standalone": {
+                    "standalone_system_notice",
+                    "standalone_validation_notice",
+                },
+                "with_filename": {
+                    "filename": {
+                        "with_filename_system_notice",
+                        "with_filename_validation_notice",
+                    },
+                    "another_filename_validation": {
+                        "another_validation_filename_notice"
+                    },
+                    "child_filename": {
+                        "with_child_filename_validation_notice",
+                        "with_child_filename_system_notice",
+                    },
+                    "another_filename_system": {"another_system_filename_notice"},
                 },
             },
         )
