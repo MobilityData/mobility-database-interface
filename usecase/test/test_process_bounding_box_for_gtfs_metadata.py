e0abf228d55e1631d8f67a1f023f28d44cd657c6
Maxime Armstrong <info@maximearmstrong.com>
2021-04-19 16:57:23 -0400

    Modifies tests for geographical boundaries use cases

--- usecase/test/test_process_bounding_box_for_gtfs_metadata.py
+++ usecase/test/test_process_bounding_box_for_gtfs_metadata.py
@@ -6,6 +6,8 @@ from representation.gtfs_metadata import GtfsMetadata
 from representation.gtfs_representation import GtfsRepresentation
 from usecase.process_geopraphical_boundaries_for_gtfs_metadata import (
     process_bounding_box_for_gtfs_metadata,
+    STOP_LAT,
+    STOP_LON,
 )
 from utilities.geographical_utils import LAT, LON
 
@@ -25,21 +27,60 @@ class TestProcessBoundingBoxForGtfsMetadata(TestCase):
             TypeError, process_bounding_box_for_gtfs_metadata, mock_gtfs_representation
         )
 
-    @mock.patch("representation.gtfs_metadata.GtfsMetadata")
-    @mock.patch("gtfs_kit.feed.Feed")
-    @mock.patch("representation.gtfs_representation.GtfsRepresentation")
-    def test_process_bounding_box_execution_should_set_bounding_box_metadata(
-        self, mock_gtfs_representation, mock_dataset, mock_metadata
-    ):
-        mock_stops = PropertyMock(
-            return_value=pd.DataFrame(
-                {"stop_lat": [45.508888], "stop_lon": [-73.561668]}
-            )
+    def test_process_bounding_box_execution_missing_files(self):
+        mock_dataset = MagicMock()
+        mock_dataset.__class__ = Feed
+
+        mock_metadata = MagicMock()
+        mock_metadata.__class__ = GtfsMetadata
+
+        mock_gtfs_representation = MagicMock()
+        mock_gtfs_representation.__class__ = GtfsRepresentation
+        type(mock_gtfs_representation).dataset = mock_dataset
+        type(mock_gtfs_representation).metadata = mock_metadata
+
+        under_test = process_bounding_box_for_gtfs_metadata(mock_gtfs_representation)
+        self.assertIsInstance(under_test, GtfsRepresentation)
+        self.assertEqual(
+            mock_metadata.bounding_box,
+            {},
         )
+
+    def test_process_bounding_box_execution_missing_fields(self):
+        mock_stops = PropertyMock(return_value=pd.DataFrame({}))
+        mock_dataset = MagicMock()
         mock_dataset.__class__ = Feed
         type(mock_dataset).stops = mock_stops
 
+        mock_metadata = MagicMock()
         mock_metadata.__class__ = GtfsMetadata
+
+        mock_gtfs_representation = MagicMock()
+        mock_gtfs_representation.__class__ = GtfsRepresentation
+        type(mock_gtfs_representation).dataset = mock_dataset
+        type(mock_gtfs_representation).metadata = mock_metadata
+
+        under_test = process_bounding_box_for_gtfs_metadata(mock_gtfs_representation)
+        self.assertIsInstance(under_test, GtfsRepresentation)
+        self.assertEqual(
+            mock_metadata.bounding_box,
+            {},
+        )
+
+    def test_process_bounding_box_execution_should_set_bounding_box_metadata(
+        self,
+    ):
+        mock_stops = PropertyMock(
+            return_value=pd.DataFrame({STOP_LAT: [45.508888], STOP_LON: [-73.561668]})
+        )
+        mock_dataset = MagicMock()
+        mock_dataset.__class__ = Feed
+        type(mock_dataset).stops = mock_stops
+
+        mock_metadata = MagicMock()
+        mock_metadata.__class__ = GtfsMetadata
+
+        mock_gtfs_representation = MagicMock()
         mock_gtfs_representation.__class__ = GtfsRepresentation
         type(mock_gtfs_representation).dataset = mock_dataset
         type(mock_gtfs_representation).metadata = mock_metadata
