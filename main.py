2b475474afbbe8ce35e0dc51fc90adab6a8a952d
Annaelle <annaelle.duff@gmail.com>
2021-04-28 11:58:47 +0200

    commenting name/main

--- main.py
+++ main.py
@@ -67,119 +67,120 @@ from utilities.constants import (
 )
 
 BASE_DIR = Path(__file__).resolve().parent
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="MobilityDatabase Interface Script")
-    parser.add_argument(
-        "--data-type",
-        action="store",
-        choices=["GTFS", "GBFS"],
-        default="GTFS",
-        help='Type of the datasets to process. Possible values : "GTFS", "GBFS".',
-    )
-    parser.add_argument(
-        "--path-to-tmp-data",
-        action="store",
-        default="./data/tmp/",
-        help="Path to the folder where to temporary store downloaded datasets for processing.",
-    )
-    parser.add_argument(
-        "--path-to-env-var",
-        action="store",
-        default="./.env.staging",
-        help="Path to the environment variables.",
-    )
-    parser.add_argument(
-        "--path-to-credentials",
-        action="store",
-        default="./staging_credentials.json",
-        help="Path to the credentials.",
-    )
-    args = parser.parse_args()
-
-    # Load environment from dotenv file and credentials json file
-    load_dotenv(args.path_to_env_var)
-    with open(args.path_to_credentials) as f:
-        credentials = json.load(f)
-    os.environ[USERNAME] = credentials.get(USERNAME)
-    os.environ[PASSWORD] = credentials.get(PASSWORD)
-
-    # Get environment variables
-    sparql_url = os.environ.get(SPARQL_URL)
-    api_url = os.environ.get(API_URL)
-
-    # Load Wikibase Integrator config with the environment
-    wbi_config["MEDIAWIKI_API_URL"] = os.environ[API_URL]
-    wbi_config["SPARQL_ENDPOINT_URL"] = os.environ[SPARQL_BIGDATA_URL]
-    wbi_config["WIKIBASE_URL"] = SVC_URL
-
-    # Initialize DataRepository
-    data_repository = DataRepository()
-
-    # Process data
-    # Download datasets zip files
-    datasets_infos = extract_gtfs_datasets_infos_from_database(
-        api_url,
-        sparql_url,
-    )
-
-    # Download datasets zip files
-    datasets_infos = download_datasets_as_zip(args.path_to_tmp_data, datasets_infos)
-
-    # Process the MD5 hashes
-    datasets_infos = process_datasets_md5(datasets_infos)
-
-    # Load the datasets in memory in the data repository
-    data_repository = load_datasets(data_repository, datasets_infos, args.data_type)
-
-    # Process each dataset representation in the data_repository
-    for (
-        dataset_key,
-        dataset_representation,
-    ) in data_repository.get_dataset_representations().items():
-        dataset_representation = process_start_service_date_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_end_service_date_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_start_timestamp_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_end_timestamp_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_main_language_code_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_timezones_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_bounding_box_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_bounding_octagon_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_agencies_count_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_routes_count_by_type_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = process_stops_count_by_type_for_gtfs_metadata(
-            dataset_representation
-        )
-        dataset_representation = create_dataset_entity_for_gtfs_metadata(
-            dataset_representation, api_url
-        )
-
-        # Print results
-        data_repository.print_dataset_representation(dataset_key)
-
-    # Print memory usage
-    print("\n--------------- Memory Usage ---------------\n")
-    print(hpy().heap())
+#
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description="MobilityDatabase Interface Script")
+#     parser.add_argument(
+#         "--data-type",
+#         action="store",
+#         choices=["GTFS", "GBFS"],
+#         default="GTFS",
+#         help='Type of the datasets to process. Possible values : "GTFS", "GBFS".',
+#     )
+#     parser.add_argument(
+#         "--path-to-tmp-data",
+#         action="store",
+#         default="./data/tmp/",
+#         help="Path to the folder where to temporary store downloaded datasets for processing.",
+#     )
+#     parser.add_argument(
+#         "--path-to-env-var",
+#         action="store",
+#         default="./.env.staging",
+#         help="Path to the environment variables.",
+#     )
+#     parser.add_argument(
+#         "--path-to-credentials",
+#         action="store",
+#         default="./staging_credentials.json",
+#         help="Path to the credentials.",
+#     )
+#     args = parser.parse_args()
+#
+#     # Load environment from dotenv file and credentials json file
+#     load_dotenv(args.path_to_env_var)
+#     with open(args.path_to_credentials) as f:
+#         credentials = json.load(f)
+#     os.environ[USERNAME] = credentials.get(USERNAME)
+#     os.environ[PASSWORD] = credentials.get(PASSWORD)
+#
+#     # Get environment variables
+#     sparql_url = os.environ.get(SPARQL_URL)
+#     api_url = os.environ.get(API_URL)
+#
+#     # Load Wikibase Integrator config with the environment
+#     wbi_config["MEDIAWIKI_API_URL"] = os.environ[API_URL]
+#     wbi_config["SPARQL_ENDPOINT_URL"] = os.environ[SPARQL_BIGDATA_URL]
+#     wbi_config["WIKIBASE_URL"] = SVC_URL
+#
+#     # Initialize DataRepository
+#     data_repository = DataRepository()
+#
+#     # Process data
+#     # Download datasets zip files
+#     datasets_infos = extract_gtfs_datasets_infos_from_database(
+#         api_url,
+#         sparql_url,
+#     )
+#
+#     # Download datasets zip files
+#     datasets_infos = download_datasets_as_zip(args.path_to_tmp_data, datasets_infos)
+#
+#     # Process the MD5 hashes
+#     datasets_infos = process_datasets_md5(datasets_infos)
+#
+#     # Load the datasets in memory in the data repository
+#     data_repository = load_datasets(data_repository, datasets_infos, args.data_type)
+#
+#     # Process each dataset representation in the data_repository
+#     for (
+#         dataset_key,
+#         dataset_representation,
+#     ) in data_repository.get_dataset_representations().items():
+#         dataset_representation = process_start_service_date_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_end_service_date_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_start_timestamp_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_end_timestamp_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_main_language_code_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_timezones_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_bounding_box_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_bounding_octagon_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_agencies_count_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_routes_count_by_type_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = process_stops_count_by_type_for_gtfs_metadata(
+#             dataset_representation
+#         )
+#         dataset_representation = create_dataset_entity_for_gtfs_metadata(
+#             dataset_representation, api_url
+#         )
+#
+#         # Print results
+#         data_repository.print_dataset_representation(dataset_key)
+#
+#     # Print memory usage
+#     print("\n--------------- Memory Usage ---------------\n")
+#     print(hpy().heap())
+#
 
 
 def add_new_source_cf(event, context):
